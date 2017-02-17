c389fb301915c0ced1340473f6c79fb712770a25
Thomas Trinqué <thomas.trinque@gmail.com>
2017-02-17 13:40:11 +0100

    push

--- app/components/allListsController.js
+++ app/components/allListsController.js
@@ -25,6 +25,8 @@ angular.module("allLists").controller("allListsController", ["$http", "$scope", 
     this.research = "";
   }
 
+  this.affichage = "owner";
+
   this.viewerowner = () => {
     if(this.affichage === "owner") {
       return true;
